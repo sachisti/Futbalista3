3c06fda00ccf222ce5bce4e41a1066e0820aea8f
BrunoSurman <bruno.surman@gmail.com>
2023-04-07 19:06:37 +0200

    nastavenie senzorov a motorov podla siveho robota

--- 2023/arduino/2023/futbalista.h
+++ 2023/arduino/2023/futbalista.h
@@ -25,6 +25,10 @@ void motor_smer(uint8_t motor, uint8_t smer);
 void test_motors2();
 void test_motors();
 
+void test_left_motor();
+void test_right_motor();
+void test_back_motor();
+
 
 // vypinace
 
--- 2023/arduino/2023/komunikacia.cpp
+++ 2023/arduino/2023/komunikacia.cpp
@@ -50,6 +50,13 @@ void spracuj_paket()
   else if (a == 22) dolava();
   else if (a == 23) dopredu();
   else if (a == 24) dokola();
+
+  else if (a == 41) test_left_motor();
+  else if (a == 42) test_right_motor();
+  else if (a == 43) test_back_motor();
+
+  else if (a == 45) setup_komunikacia();
+  
 }
 
 void precitaj_dalsi_znak()
